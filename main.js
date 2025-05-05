017582de8fb3c1ece998e1322785e25d31d54605
Francesco Viccica <viccicafrancesco@gmail.com>
2025-05-05 10:39:48 +0200

    correzione

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
 
 // ordine decrescente
 
-numbers.sort( (a, b) => b - a );
+let decrescente = numbers.sort( (a, b) => b - a );
 
-console.log(numbers);
+console.log(decrescente);
 
 // Ordine crescente
 
-numbers.sort((a, b) => a - b );
+let crescente = numbers.sort((a, b) => a - b );
 
-console.log(numbers);
+console.log(crescente);
+
+
+
 
 

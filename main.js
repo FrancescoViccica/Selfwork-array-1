0240c545498e1476df9cd1419a3db8975eb734fa
Francesco Viccica <viccicafrancesco@gmail.com>
2025-05-05 10:32:41 +0200

    esercizio svolto

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('ciao');
+let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
+
+// ordine decrescente
+
+numbers.sort( (a, b) => b - a );
+
+console.log(numbers);
+
+// Ordine crescente
+
+numbers.sort((a, b) => a - b );
+
+console.log(numbers);
+
+
